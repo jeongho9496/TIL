d0a1d708494f9a06b145bb644bc26332f4e43e50
jeongho9496 <jeongho9496@douzone.com>
2022-03-20 23:16:39 +0900

    hello world

---
+++ Programmers/Level1/js/test.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+function btn_alert() {
+    let str = 'Hello World !!'
+    alert(str);
+}
+
+// function solution(id_list, report, k) {
+//     var answer = [];
+//     return answer;
+// }
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
